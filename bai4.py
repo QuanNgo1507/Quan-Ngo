7caf0f8075c43c16a84e3d1c14343dc698adca4b
QuanNgo1507 <quanngoduy1507@gmail.com>
2026-03-19 15:32:30 +0700

    Delete bai4.py

--- bai4.py
+++
@@ -1,8 +0,0 @@
-class Hoa:
-    def __init__(self, ten, mau):
-        self.ten = ten
-        self.mau = mau
-    def __str__(self):
-        return f"Hoa {self.ten} có màu {self.mau}."
-hoa_cua_toi = Hoa("Hồng", "Đỏ rực")
-print(hoa_cua_toi)
